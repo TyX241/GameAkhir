ade32af702209f177b4cfa180d78ae3ee492a731
Ninda <anindhitafaiza@gmail.com>
2025-05-23 14:16:51 +0700

    Update PembeliStandard.java

--- GameAkhir/PembeliStandard.java
+++ GameAkhir/PembeliStandard.java
@@ -1,5 +1,23 @@
 package GameAkhir;
 
+import java.util.Random;
+
 public class PembeliStandard extends Pembeli {
-    
+    private final Random random = new Random();
+
+    public PembeliStandard(String nama) {
+        super(nama);
+    }
+
+    @Override
+    public int tawarHarga(int hargaAwal) {
+        int persen = 70 + random.nextInt(21); 
+        return hargaAwal * persen / 100;
+    }
+
+    @Override
+    public boolean putuskanBeli() {
+        return random.nextDouble() < 0.65;
+    }
 }
+
