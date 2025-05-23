8a5b444539343af5ea14782b5847f78a7b77b6e1
Ninda <anindhitafaiza@gmail.com>
2025-05-23 14:16:23 +0700

    Update PembeliTajir.java

--- GameAkhir/PembeliTajir.java
+++ GameAkhir/PembeliTajir.java
@@ -1,5 +1,23 @@
 package GameAkhir;
 
+import java.util.Random;
+
 public class PembeliTajir extends Pembeli {
-    
+    private final Random random = new Random();
+
+    public PembeliTajir(String nama) {
+        super(nama);
+    }
+
+    @Override
+    public int tawarHarga(int hargaAwal) {
+        if (random.nextDouble() < 0.8) return hargaAwal;
+        int diskon = 5 + random.nextInt(6); 
+        return hargaAwal * (100 - diskon) / 100;
+    }
+
+    @Override
+    public boolean putuskanBeli() {
+        return random.nextDouble() < 0.95;
+    }
 }
