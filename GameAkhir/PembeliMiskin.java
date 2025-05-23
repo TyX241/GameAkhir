014a99c6e40417751604d4bc4ae16ebd579ded85
Ninda <anindhitafaiza@gmail.com>
2025-05-23 14:17:18 +0700

    Update PembeliMiskin.java

--- GameAkhir/PembeliMiskin.java
+++ GameAkhir/PembeliMiskin.java
@@ -1,5 +1,22 @@
 package GameAkhir;
 
-public class PembeliMiskin extends Pembeli{
-    
+import java.util.Random;
+
+public class PembeliStandard extends Pembeli {
+    private final Random random = new Random();
+
+    public PembeliStandard(String nama) {
+        super(nama);
+    }
+
+    @Override
+    public int tawarHarga(int hargaAwal) {
+        int persen = 70 + random.nextInt(21); 
+        return hargaAwal * persen / 100;
+    }
+
+    @Override
+    public boolean putuskanBeli() {
+        return random.nextDouble() < 0.65;
+    }
 }
